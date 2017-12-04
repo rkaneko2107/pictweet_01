ef4ce2237b9619b16f5fe3675b589df26248db3c
tech-camp <>
2017-12-04 14:41:48 +0900

    first commit

---
+++ src/main/java/com/example/Pictweet01Application.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pictweet01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pictweet01Application.class, args);
+	}
+}
